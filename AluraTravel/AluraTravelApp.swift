6bd6a7bc3f0fc42d15925e09103f498e3c815b22
Felipe da Silva Santos <felipedssantos811@gmail.com>
2022-11-25 15:29:27 -0300

    Initial Commit

---
+++ AluraTravel/AluraTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AluraTravelApp.swift
+//  AluraTravel
+//
+//  Created by Felipe Santos on 25/11/22.
+//
+
+import SwiftUI
+
+@main
+struct AluraTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
