9c70fc6b81938ed34f9e39a352ff05f4e3960dad
Felipe da Silva Santos <felipedssantos811@gmail.com>
2022-11-26 19:36:02 -0300

    Improve app header

--- AluraTravel/ContentView.swift
+++ AluraTravel/ContentView.swift
@@ -9,16 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack() {
-            Text("Alura Travel")
-            Text("SPECIAL")
-            Text("BRAZIL")
-            
-            List {
-                Text("Rio de Janeiro")
-                Text("Ceará")
-                Text("Atibaia")
-                Text("Rio de Janeiro")
+        GeometryReader { view in
+            VStack() {
+                VStack() {
+                    Text("alura travel")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Black", size: 24.0))
+                    Text("SPECIAL")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Book", size: 24.0))
+                        .frame(minWidth: 0.0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 32.0)
+                    Text("BRAZIL")
+                        .foregroundColor(.white)
+                        .font(.custom("Avenir Black", size: 24.0))
+                        .frame(minWidth: 0.0, maxWidth: .infinity, alignment: .leading)
+                        .padding(.leading, 32.0)
+                        .padding(.bottom, 16.0)
+                }
+                .background(.purple)
+                .frame(width: view.size.width)
+                
+                List {
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("Rio de Janeiro")
+                }
             }
         }
     }
